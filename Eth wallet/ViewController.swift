1c36d995662a9d3434de194ecffd9c24154bffad
Ivan Ermak <ermak.ivan.iv@gmail.com>
2019-05-11 12:38:20 +0300

    Initial Commit

---
+++ Eth wallet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Eth wallet
+//
+//  Created by Ivan Ermak on 5/11/19.
+//  Copyright © 2019 Ivan Ermak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
